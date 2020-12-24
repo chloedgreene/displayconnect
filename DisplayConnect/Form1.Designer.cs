b80c35bfb6a2b1f79e5b5a9dcd3ea7ffff2cc367
Connor Greene <56372117+supercatmangod@users.noreply.github.com>
2020-12-24 09:58:43 -0800

    Added Software Files

---
+++ DisplayConnect/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿
+namespace DisplayControler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(405, 89);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Select Color";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(405, 118);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 20);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Send";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(405, 170);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(75, 28);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(403, 154);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Current Colour:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(405, 223);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Clear";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(405, 253);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Message Data:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(108, 408);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(170, 17);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Write color on grid button click";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 449);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(493, 23);
+            this.progressBar1.TabIndex = 8;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(380, 395);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(380, 376);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "IP:";
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.checkBox2);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Location = new System.Drawing.Point(486, 33);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(123, 392);
+            this.panel1.TabIndex = 11;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(17, 215);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(77, 17);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "Vibe Mode";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 13);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(102, 13);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Send Scrolling Text:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 14);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 2;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 56);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(46, 23);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "Send";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 30);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(483, 17);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Advanced Options:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(640, 484);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "DisplayConnect";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ DisplayConnect/Form1.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Sc.Util.Rendering;
+
+namespace DisplayControler
+{
+    public partial class Form1 : Form
+    {
+
+        private static readonly HttpClient client = new HttpClient();
+        string RaspberryPI_IP = "";
+        List<Button> buttons = new List<Button>();
+        Color SelectedColour = Color.White;
+
+        public Button GetButton(int x,int y)
+        {
+
+            return buttons[x*8+y];
+
+        }
+
+        float map(float s, float a1, float a2, float b1, float b2)
+        {
+            return b1 + (s - a1) * (b2 - b1) / (a2 - a1);
+        }
+
+        private static String HexConverter(System.Drawing.Color c)
+        {
+            return c.R.ToString() + "," + c.G.ToString() + "," + c.B.ToString();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            int ButtonWidth = 40;
+            int ButtonHeight = 40;
+            int Distance = 20;
+            int start_x = 10;
+            int start_y = 10;
+
+            for (int x = 0; x < 8; x++)
+            {
+                for (int y = 0; y < 8; y++)
+                {
+                    Button tmpButton = new Button();
+                    tmpButton.Top = start_x + (x * ButtonHeight + Distance);
+                    tmpButton.Left = start_y + (y * ButtonWidth + Distance);
+                    tmpButton.Width = ButtonWidth;
+                    tmpButton.Height = ButtonHeight;
+                    tmpButton.BackColor = Color.White;
+                    tmpButton.Text = y + "," + x;
+                    // Possible add Buttonclick event etc..
+
+                    tmpButton.Click += new EventHandler(gridbutton_Click);
+
+                    buttons.Add(tmpButton);
+
+                    this.Controls.Add(tmpButton);
+
+                }
+
+            }
+
+            //button2.Hide();
+
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("plain/text"));
+            //Console.WriteLine(client.GetStringAsync($"http://{RaspberryPI_IP}:1880/api/init").Result);
+
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = 64;
+
+            timer1.Tick += dispatcherTimer_Tick;
+            timer1.Interval = 1;
+            timer1.Start();
+
+            timer2.Tick += offsetTimer_Tick;
+            timer2.Interval = 20;
+            timer2.Start();
+
+        }
+
+        private void offsetTimer_Tick(object sender, EventArgs e) {
+
+
+
+        }
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+
+
+            if (checkBox2.Checked) {
+
+                //checkBox1.Checked = true;
+                int i = 0;
+
+                foreach (Button b in buttons) {
+
+                    i++;
+
+                    
+
+                    SelectedColour = SimpleColorTransforms.HsLtoRgb(map(i,0,64,0,360),98,0.5);
+
+                    b.BackColor = SelectedColour;
+
+                    int x = Int32.Parse(b.Text.Split(',')[0]);
+                    int y = Int32.Parse(b.Text.Split(',')[1]);
+
+                    
+
+
+
+                }
+
+               // Console.WriteLine("Working");
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.BackColor = colorDialog1.Color;
+
+                SelectedColour = colorDialog1.Color;
+            }
+        }
+
+        private void gridbutton_Click(object sender, EventArgs e) {
+
+
+            Button OrgButton = (Button)sender;
+
+            OrgButton.BackColor = SelectedColour;
+
+            if (checkBox1.Checked) {
+
+                int x = Int32.Parse(OrgButton.Text.Split(',')[0]);
+                int y = Int32.Parse(OrgButton.Text.Split(',')[1]);
+
+
+
+
+
+                try
+                {
+                    Console.WriteLine(client.GetStringAsync($"http://{RaspberryPI_IP}:1880/api/pixel?x={x}&y={y}&c={OrgButton.BackColor.R},{OrgButton.BackColor.G},{OrgButton.BackColor.B}").Result);
+                }
+                catch
+                {
+                    
+                    MessageBox.Show("INVALID IP OR NETWORK ERROR");
+                    return;
+                }
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+
+
+            
+            foreach(Button b in buttons)
+            {
+
+                    int x = Int32.Parse(b.Text.Split(',')[0]);
+                    int y = Int32.Parse(b.Text.Split(',')[1]);
+
+                try
+                {
+                    Console.WriteLine(client.GetStringAsync($"http://{RaspberryPI_IP}:1880/api/pixel?x={x}&y={y}&c={b.BackColor.R},{b.BackColor.G},{b.BackColor.B}").Result);
+                }
+                catch {
+
+                    MessageBox.Show("INVALID IP OR NETWORK ERROR");
+                    break;
+                }
+                    //Thread.Sleep(25);
+
+
+                progressBar1.Value++;
+
+            }
+            Thread.Sleep(250);
+            progressBar1.Value = 0;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            foreach (Button b in buttons) {
+
+                b.BackColor = Color.White;
+
+                b.PerformClick();
+
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            RaspberryPI_IP = textBox1.Text;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Console.WriteLine(client.GetStringAsync($"http://{RaspberryPI_IP}:1880/api/text?t={textBox2.Text}").Result);
+            }
+            catch
+            {
+
+                MessageBox.Show("INVALID IP OR NETWORK ERROR");
+               
+            }
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
